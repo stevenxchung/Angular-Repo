d8ab52b3b08576808b34abd9e7dda3f13bc83206
Steven <stevenxchung@gmail.com>
2018-06-13 13:30:31 -0400

    Fix minor issues with references

--- Angular 5/Tutorials/angular-d3-example/src/app/d3/d3.service.ts
+++ Angular 5/Tutorials/angular-d3-example/src/app/d3/d3.service.ts
@@ -1,35 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Node, Link, ForceDirectedGraph } from './models';
 import * as d3 from 'd3';
 
 @Injectable()
 export class D3Service {
-    /** This service will provide methods to enable user interaction with elements
+  /** This service will provide methods to enable user interaction with elements
     * while maintaining the d3 simulations physics
     */
-    constructor() {}
+  constructor() { }
 
-    /** A method to bind a pan and zoom behaviour to an svg element */
-    applyZoomableBehaviour(svgElement, containerElement) {
-        let svg, container, zoomed, zoom;
+  /** A method to bind a pan and zoom behaviour to an svg element */
+  applyZoomableBehaviour(svgElement, containerElement) {
+    let svg, container, zoomed, zoom;
 
-        svg = d3.select(svgElement);
-        container = d3.select(containerElement);
+    svg = d3.select(svgElement);
+    container = d3.select(containerElement);
 
-        zoomed = () => {
-          const transform = d3.event.transform;
-          container.attr("transform", "translate(" + transform.x + "," + transform.y + ") scale(" + transform.k + ")");
-        }
-
-        zoom = d3.zoom().on("zoom", zoomed);
-        svg.call(zoom);
+    zoomed = () => {
+      const transform = d3.event.transform;
+      container.attr('transform', 'translate(' + transform.x + ',' + transform.y + ') scale(' + transform.k + ')');
     }
 
+    zoom = d3.zoom().on('zoom', zoomed);
+    svg.call(zoom);
+  }
 
-    /** A method to bind a draggable behaviour to an svg element */
-    applyDraggableBehaviour(element, node: Node, graph: ForceDirectedGraph) {
-      const d3element = d3.select(element);
+  /** A method to bind a draggable behaviour to an svg element */
+  applyDraggableBehaviour(element, node: Node, graph: ForceDirectedGraph) {
+    const d3element = d3.select(element);
 
-      function started() {
+    function started() {
       /** Preventing propagation of dragstart to parent elements */
       d3.event.sourceEvent.stopPropagation();
 
@@ -37,7 +37,7 @@ export class D3Service {
         graph.simulation.alphaTarget(0.3).restart();
       }
 
-      d3.event.on("drag", dragged).on("end", ended);
+      d3.event.on('drag', dragged).on('end', ended);
 
       function dragged() {
         node.fx = d3.event.x;
@@ -55,14 +55,14 @@ export class D3Service {
     }
 
     d3element.call(d3.drag()
-      .on("start", started));
-    }
+      .on('start', started));
+  }
 
-    /** The interactable graph we will simulate in this article
-    * This method does not interact with the document, purely physical calculations with d3
-    */
-    getForceDirectedGraph(nodes: Node[], links: Link[], options: { width, height} ) {
-    let graph = new ForceDirectedGraph(nodes, links, options);
-    return graph;
-}
+  /** The interactable graph we will simulate in this article
+  * This method does not interact with the document, purely physical calculations with d3
+  */
+  getForceDirectedGraph(nodes: Node[], links: Link[], options: { width, height }) {
+    const sg = new ForceDirectedGraph(nodes, links, options);
+    return sg;
+  }
 }
