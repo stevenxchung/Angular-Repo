d82ebac6028f13e4f4ae27379810acddb6f24f2d
Steven <stevenxchung@gmail.com>
2018-06-14 22:45:07 -0400

    Set up server

--- Angular 5/Tutorials/Angular 5 with D3/server.js
+++ Angular 5/Tutorials/Angular 5 with D3/server.js
@@ -1,5 +1,16 @@
+// Import packages
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(_dirname + 'dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+
+res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
