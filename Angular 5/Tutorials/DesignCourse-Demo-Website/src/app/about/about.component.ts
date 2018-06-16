37c487ee98e8c976060d21c48b27c18d8a6766a2
Steven <stevenxchung@gmail.com>
2018-06-16 12:32:47 -0400

    Provide routing to about page

--- Angular 5/Tutorials/DesignCourse-Demo-Website/src/app/about/about.component.ts
+++ Angular 5/Tutorials/DesignCourse-Demo-Website/src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// Gives access to the route parameters like /:id
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  // Dependency injection happens within constructor()
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
+  }
+
+  // Callback that navigates to the home page
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
