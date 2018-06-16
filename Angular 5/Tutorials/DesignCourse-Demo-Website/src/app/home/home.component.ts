0477d724f987bdc5d2299cca5e6bde86ba9cd58d
Steven <stevenxchung@gmail.com>
2018-06-16 12:09:45 -0400

    Add animations for adding and removing elements to list

--- Angular 5/Tutorials/DesignCourse-Demo-Website/src/app/home/home.component.ts
+++ Angular 5/Tutorials/DesignCourse-Demo-Website/src/app/home/home.component.ts
@@ -1,16 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+// Import animations
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+        transition('* => *', [
+            query(':enter', style({ opacity: 0}), {optional: true}),
+
+            // Animation when adding an element to the list
+            query(':enter', stagger('300ms', [
+              animate('.6s ease-in', keyframes([
+                style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+
+              ]))
+            ]), {optional: true}),
+
+            // Animation when removing an element from the list
+            query(':leave', stagger('300ms', [
+              animate('.6s ease-in', keyframes([
+                style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+
+              ]))
+            ]), {optional: true})
+        ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = [
+    'Climb half dome',
+    'Become a millionaire',
+    'Travel the world'
+  ];
 
   constructor() { }
 
@@ -23,6 +64,11 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  // Remove item from list
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
