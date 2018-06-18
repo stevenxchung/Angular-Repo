67c32184420e7fde4b11b71b3416271ec0e980c0
Steven <stevenxchung@gmail.com>
2018-06-18 17:06:16 -0400

    Fixed bug for user details not rendering

--- Angular/Tutorials/DesignCourse-Angular-6-Demo/src/app/details/details.component.ts
+++ Angular/Tutorials/DesignCourse-Angular-6-Demo/src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
-import { ActivatedRoute, Data } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
