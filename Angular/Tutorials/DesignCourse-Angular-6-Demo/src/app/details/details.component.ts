0ea46f9bd0748117bfa7356dd0c26e4ad5cd2b32
Steven <stevenxchung@gmail.com>
2018-06-18 17:24:17 -0400

    Add commentary for ActivatedRoute

--- Angular/Tutorials/DesignCourse-Angular-6-Demo/src/app/details/details.component.ts
+++ Angular/Tutorials/DesignCourse-Angular-6-Demo/src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
+// We use ActivatedRoute because of the :id after details/
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
