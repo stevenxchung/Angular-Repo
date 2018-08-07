6898573dfadce61213ce001092730988fb2c70f8
Steven <stevenxchung@gmail.com>
2018-08-07 19:05:18 -0400

    Append arrays example

--- Angular/Tutorials/RxJS/ng-europe-2016-observables.js
+++ Angular/Tutorials/RxJS/ng-europe-2016-observables.js
@@ -9,3 +9,12 @@ function consoleLogClick(x) {
 }
 
 myElem.addEventListener('click', consoleLogClick);
+
+// Example 2: Arrays
+const arr = [10, 20, 30, 40, 50, 60];
+
+console.log('before');
+arr.forEach(function cb(x) {
+  console.log(x);
+});
+console.log('after');
