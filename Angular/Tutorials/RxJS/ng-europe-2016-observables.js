addd28c0e1d76d814d7bb20d4c7cf3251ad9e2d0
Steven <stevenxchung@gmail.com>
2018-08-08 18:45:24 -0400

    Append example 5 (step 5) - activate click observable

--- Angular/Tutorials/RxJS/ng-europe-2016-observables.js
+++ Angular/Tutorials/RxJS/ng-europe-2016-observables.js
@@ -218,3 +218,67 @@ const observer = {
 
 // Invoking subscribe()
 arrayObservable.map(x => x / 10).filter(x => x !== 2).subscribe(observer);
+
+// Example 5: Generic API - Step 5
+function filter(conditionFn) {
+  const inputObservable = this;
+  const outputObservable = createObservable(function subscribe(outputObserver) {
+    inputObservable.subscribe({
+      next: function(x) {
+        if (conditionFn(x)) {
+          outputObserver.next(x);
+        }
+      },
+      error: e => outputObserver.error(e),
+      complete: () => outputObserver.complete()
+    });
+  });
+  return outputObservable;
+}
+
+function map(transformFn) {
+  const inputObservable = this;
+  const outputObservable = createObservable(function subscribe(outputObserver) {
+    inputObservable.subscribe({
+      next: function(x) {
+        const y = transformFn(x);
+        outputObserver.next(y);
+      },
+      error: e => outputObserver.error(e),
+      complete: () => outputObserver.complete()
+    });
+  });
+  return outputObservable;
+}
+
+function createObservable(subscribe) {
+  return {
+    subscribe: subscribe,
+    map: map,
+    filter: filter
+  };
+}
+
+const clickObservable = createObservable(function subscribe(ob) {
+  document.addEventListener("click", ob.next);
+});
+
+const arrayObservable = createObservable(function subscribe(ob) {
+  [10, 20, 30].forEach(ob.next);
+  ob.complete();
+});
+
+const observer = {
+  next: function nextCallback(data) {
+    console.log(data);
+  },
+  error: function errorCallback(err) {
+    console.log(err);
+  },
+  complete: function completeCallback() {
+    console.log("done");
+  }
+};
+
+// Invoking subscribe()
+clickObservable.map(ev => ev.clientX).filter(x => x < 200).subscribe(observer);
