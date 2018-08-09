39ea3d046a6152e0c3992d84169b523d3ca35d5b
Steven <stevenxchung@gmail.com>
2018-08-09 18:40:13 -0400

    Add delay to observable and final packaged RxJS to tutorial

--- Angular/Tutorials/RxJS/ng-europe-2016-observables.js
+++ Angular/Tutorials/RxJS/ng-europe-2016-observables.js
@@ -217,7 +217,10 @@ const observer = {
 };
 
 // Invoking subscribe()
-arrayObservable.map(x => x / 10).filter(x => x !== 2).subscribe(observer);
+arrayObservable
+  .map(x => x / 10)
+  .filter(x => x !== 2)
+  .subscribe(observer);
 
 // Example 5: Generic API - Step 5
 function filter(conditionFn) {
@@ -281,4 +284,110 @@ const observer = {
 };
 
 // Invoking subscribe()
-clickObservable.map(ev => ev.clientX).filter(x => x < 200).subscribe(observer);
+clickObservable
+  .map(ev => ev.clientX)
+  .filter(x => x < 200)
+  .subscribe(observer);
+
+// Example 5: Generic API - Step 6
+function delay(period) {
+  const inputObservable = this;
+  const outputObservable = createObservable(function subscribe(outputObserver) {
+    inputObservable.subscribe({
+      next: function(x) {
+        setTimeout(function() {
+          outputObserver.next(x);
+        }, period);
+      },
+      error: e => outputObserver.error(e),
+      complete: () => outputObserver.complete()
+    });
+  });
+  return outputObservable;
+}
+
+function filter(conditionFn) {
+  const inputObservable = this;
+  const outputObservable = createObservable(function subscribe(outputObserver) {
+    inputObservable.subscribe({
+      next: function(x) {
+        if (conditionFn(x)) {
+          outputObserver.next(x);
+        }
+      },
+      error: e => outputObserver.error(e),
+      complete: () => outputObserver.complete()
+    });
+  });
+  return outputObservable;
+}
+
+function map(transformFn) {
+  const inputObservable = this;
+  const outputObservable = createObservable(function subscribe(outputObserver) {
+    inputObservable.subscribe({
+      next: function(x) {
+        const y = transformFn(x);
+        outputObserver.next(y);
+      },
+      error: e => outputObserver.error(e),
+      complete: () => outputObserver.complete()
+    });
+  });
+  return outputObservable;
+}
+
+function createObservable(subscribe) {
+  return {
+    subscribe: subscribe,
+    map: map,
+    filter: filter,
+    delay: delay
+  };
+}
+
+const clickObservable = createObservable(function subscribe(ob) {
+  document.addEventListener("click", ob.next);
+});
+
+const arrayObservable = createObservable(function subscribe(ob) {
+  [10, 20, 30].forEach(ob.next);
+  ob.complete();
+});
+
+const observer = {
+  next: function nextCallback(data) {
+    console.log(data);
+  },
+  error: function errorCallback(err) {
+    console.log(err);
+  },
+  complete: function completeCallback() {
+    console.log("done");
+  }
+};
+
+// Invoking subscribe()
+clickObservable
+  .map(ev => ev.clientX)
+  .filter(x => x < 200)
+  .delay(2000)
+  .subscribe(observer);
+
+// Example 6: RxJS Shipped
+import { Observable } from "rxjs";
+
+const numbersObservable = Observable.from([10, 20, 30, 40, 50, 60]);
+
+numbersObservable
+  .filter(x => x !== 20)
+  .map(x => x / 10)
+  .subscribe({
+    next: function next(x) {
+      console.log(x);
+    },
+    error: function error(err) {},
+    complete: function complete() {
+      console.log("done");
+    }
+  });
