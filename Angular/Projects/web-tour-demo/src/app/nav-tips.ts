845ed1d29878ce9fe6be2c72d822457243dc3181
Steven <stevenxchung@gmail.com>
2018-08-19 10:45:09 -0400

    Add nav tips array

---
+++ Angular/Projects/web-tour-demo/src/app/nav-tips.ts
@@ -0,0 +1,7 @@
+export const NavTips: Array<any> = [
+  'Welcome to the navigation menu! Click right to navigate through the website!',
+  'Click the "Home" button to navigate from another page back home!',
+  'Click the "Features" button to navigate to the features page!',
+  'Click the "Pricing" button to navigate to the pricing page!',
+  'Click the "About" button to navigate to the about page!'
+];
