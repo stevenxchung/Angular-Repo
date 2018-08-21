773eb24cfaeb6b086128b5cc84c56c086b8ebbff
Steven <stevenxchung@gmail.com>
2018-08-20 21:17:26 -0400

    Add more constants to the tour

--- Angular/Projects/web-tour-demo/src/app/nav-tips.ts
+++ Angular/Projects/web-tour-demo/src/app/nav-tips.ts
@@ -3,5 +3,24 @@ export const NavTips: Array<any> = [
   'Click the "Home" button to navigate from another page back home!',
   'Click the "Features" button to navigate to the features page!',
   'Click the "Pricing" button to navigate to the pricing page!',
-  'Click the "About" button to navigate to the about page!'
+  'Click the "About" button to navigate to the about page!',
+  'Search anything on this site by filling out the form!'
 ];
+
+export const NavHeaders: Array<any> = [
+  'Welcome to the Tour!',
+  'Home',
+  'Features',
+  'Pricing',
+  'About',
+  'Search'
+];
+
+export const NavElements: Array<any> = [
+  '',
+  'nav-home',
+  'nav-features',
+  'nav-pricing',
+  'nav-about',
+  'nav-search'
+]
