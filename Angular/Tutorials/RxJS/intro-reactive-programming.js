8a01d33764ce49a3e88acc97a4ddb08f494447de
Steven <stevenxchung@gmail.com>
2018-09-12 19:44:44 -0400

    Transform strings into actual parsed values

--- Angular/Tutorials/RxJS/intro-reactive-programming.js
+++ Angular/Tutorials/RxJS/intro-reactive-programming.js
@@ -15,4 +15,10 @@ let source = Rx.Observable.interval(400)
   .take(9)
   .map(i => ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'][i]);
 let result = source;
+result.subscribe(x => console.log(x));
+
+// Transform into actual value
+console.clear();
+let source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
+let result = source.map(x => parseInt(x)).filter(x => !isNaN(x).reduce((x, y) => x + y));
 console.log(result);
