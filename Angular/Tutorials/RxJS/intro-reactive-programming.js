bf4485e3d0170d27ff640a902f269747f0c0a1b2
Steven <stevenxchung@gmail.com>
2018-09-12 19:30:57 -0400

    Add event stream

--- Angular/Tutorials/RxJS/intro-reactive-programming.js
+++ Angular/Tutorials/RxJS/intro-reactive-programming.js
@@ -3,10 +3,16 @@
 // An event stream is a sequence of ongoing events ordered in time which can emit three things:
 // a value, error, or complicated signal.
 
-// Synchronous
+// No streams
 console.clear();
 let source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
 let result = source;
 console.log(result);
 
-// Asynchronous
+// Event stream
+console.clear();
+let source = Rx.Observable.interval(400)
+  .take(9)
+  .map(i => ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'][i]);
+let result = source;
+console.log(result);
