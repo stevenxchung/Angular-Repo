3d08a05248284e7ab63ef3edafc9d222504c7199
Steven <stevenxchung@gmail.com>
2018-09-12 19:54:27 -0400

    Transform values for event stream

--- Angular/Tutorials/RxJS/intro-reactive-programming.js
+++ Angular/Tutorials/RxJS/intro-reactive-programming.js
@@ -17,8 +17,21 @@ let source = Rx.Observable.interval(400)
 let result = source;
 result.subscribe(x => console.log(x));
 
-// Transform into actual value
+// Transform into actual parsed value
 console.clear();
 let source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
-let result = source.map(x => parseInt(x)).filter(x => !isNaN(x).reduce((x, y) => x + y));
+let result = source
+  .map(x => parseInt(x))
+  .filter(x => !isNaN(x).reduce((x, y) => x + y));
 console.log(result);
+
+// Event stream transform
+console.clear();
+let source = Rx.Observable.interval(400)
+  .take(9)
+  .map(i => ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'][i]);
+let result = source
+  .map(x => parseInt(x))
+  .filter(x => !NaN(x))
+  .reduce((x, y) => x + y);
+result.subscribe(x => console.log(x));
