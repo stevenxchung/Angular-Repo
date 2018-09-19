131e166626e55ed78573ac0a06120e4841203929
Steven <stevenxchung@gmail.com>
2018-09-19 17:44:59 -0400

    Add reducer

---
+++ Angular/Tutorials/DesignCourse-NgRx-Tutorial/src/app/store/reducer/tutorial.reducer.ts
@@ -0,0 +1,20 @@
+import { Action } from '@ngrx/store';
+import { Tutorial } from '../../models/tutorial.model';
+import * as TutorialActions from '../action/tutorial.actions';
+
+const initialState: Tutorial = {
+  name: 'Initial Tutorial',
+  url: 'http://www.google.com'
+};
+
+export function reducer(
+  state: Tutorial[] = [initialState],
+  action: TutorialActions.Actions
+) {
+    switch(action.type) {
+        case TutorialActions.ADD_TUTORIAL:
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
