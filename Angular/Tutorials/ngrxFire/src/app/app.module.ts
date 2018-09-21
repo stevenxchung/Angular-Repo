f57f123cf380441b4b4439531c38ca19392d4974
Steven <stevenxchung@gmail.com>
2018-09-20 20:13:13 -0400

    Initialize app

--- Angular/Tutorials/ngrxFire/src/app/app.module.ts
+++ Angular/Tutorials/ngrxFire/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ message: simpleReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
