564081a94a5b11b65fbfd1129b370131fa36d59f
Steven <stevenxchung@gmail.com>
2018-09-20 20:21:13 -0400

    Fix typo

--- Angular/Tutorials/ngrxFire/src/app/simple.reducer.ts
+++ Angular/Tutorials/ngrxFire/src/app/simple.reducer.ts
@@ -3,7 +3,7 @@ import { Action } from '@ngrx/store';
 export function simpleReducer(state: string = 'Hello World', action: Action) {
   switch (action.type) {
     case 'SPANISH':
-      return (state = 'Mola Mundo');
+      return (state = 'Hola Mundo');
     case 'FRENCH':
       return (state = 'Bonjour le monde');
     default:
