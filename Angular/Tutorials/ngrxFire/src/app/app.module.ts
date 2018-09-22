5bc0ba97d15f6699149ae4c84b86819b618f88fb
Steven <stevenxchung@gmail.com>
2018-09-21 20:32:23 -0400

    Add modules

--- Angular/Tutorials/ngrxFire/src/app/app.module.ts
+++ Angular/Tutorials/ngrxFire/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { StoreModule } from '@ngrx/store';
-import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ message: simpleReducer })
+    FormsModule,
+    StoreModule.forRoot({
+      post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 // number of states to retain
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
