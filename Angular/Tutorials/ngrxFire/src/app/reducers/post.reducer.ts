ea500ecf5980acc1abd553d56bfa9842f0db2e11
Steven <stevenxchung@gmail.com>
2018-09-21 20:32:49 -0400

    Add NgRx actions, reducer, and post model

---
+++ Angular/Tutorials/ngrxFire/src/app/reducers/post.reducer.ts
@@ -0,0 +1,29 @@
+import * as PostActions from '../actions/post.actions';
+import { Post } from '../models/post.model';
+
+export type Action = PostActions.All;
+
+const defaultState: Post = {
+  text: 'Hello! I am the default state!',
+  likes: 1
+};
+
+const newState = (state, newData) => {
+  return { ...state, ...newData };
+};
+
+export function postReducer(state: Post = defaultState, action: Action) {
+  console.log(action.type, state);
+  switch (action.type) {
+    case PostActions.EDIT_TEXT:
+      return newState(state, { text: action.payload });
+    case PostActions.UPVOTE:
+      return newState(state, { likes: state.likes + 1 });
+    case PostActions.DOWNVOTE:
+      return newState(state, { likes: state.likes - 1 });
+    case PostActions.RESET:
+      return defaultState;
+    default:
+      return state;
+  }
+}
