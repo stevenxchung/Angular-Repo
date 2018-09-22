4a9849a6e6beead56afecd664d619aaba495ef76
Steven <stevenxchung@gmail.com>
2018-09-21 20:32:07 -0400

    Add app controller

--- Angular/Tutorials/ngrxFire/src/app/app.component.ts
+++ Angular/Tutorials/ngrxFire/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
 interface AppState {
-  message: string;
+  post: Post;
 }
 
 @Component({
@@ -13,17 +16,27 @@ interface AppState {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message$: Observable<string>
+  post: Observable<Post>;
+
+  text: string;
 
   constructor(private store: Store<AppState>) {
-    this.message$ = this.store.select('message')
+    this.post = this.store.select('post');
   }
 
-  spanishMessage() {
-    this.store.dispatch({type: 'SPANISH'})
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
   }
 
-  frenchMessage() {
-    this.store.dispatch({type: 'FRENCH'})
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
   }
 }
