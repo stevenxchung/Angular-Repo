0ec87d4ab9d20a1f9aec2c454efe0617137a2481
Steven <stevenxchung@gmail.com>
2018-09-23 18:44:31 -0400

    Add HttpClientModule and CoinService

--- Angular/Projects/ng5crud/src/app/app.module.ts
+++ Angular/Projects/ng5crud/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routerConfig';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
+import { CoinService } from './coin.service';
 
 @NgModule({
   declarations: [AppComponent, IndexComponent, CreateComponent, EditComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule],
+  providers: [CoinService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
