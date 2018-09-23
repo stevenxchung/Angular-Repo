a8ea4db0a751fbfb351705b4e6d365d77ef6a09a
Steven <stevenxchung@gmail.com>
2018-09-23 15:51:05 -0400

    Import index, create, and edit component

--- Angular/Projects/ng5crud/src/app/app.module.ts
+++ Angular/Projects/ng5crud/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
