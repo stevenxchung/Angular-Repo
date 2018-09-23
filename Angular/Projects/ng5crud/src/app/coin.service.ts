f9a5a4b818c1af2a9711627a5ce033315fa8fce0
Steven <stevenxchung@gmail.com>
2018-09-23 19:52:00 -0400

    Add CRUD for coin service

---
+++ Angular/Projects/ng5crud/src/app/coin.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class CoinService {
+  result: any;
+  constructor(private http: HttpClient) {}
+
+  addCoin(name, price) {
+    const uri = 'http://localhost:4000/coins/add';
+    const obj = {
+      name: name,
+      price: price
+    };
+    this.http.post(uri, obj).subscribe(res => console.log('Done'));
+  }
+
+  getCoins() {
+    const uri = 'http://localhost:4000/coins';
+    return this.http.get(uri).pipe(
+      map(res => {
+        return res;
+      })
+    );
+  }
+
+  editCoin(id) {
+    const uri = 'http://localhost:4000/coins/edit/' + id;
+    return this.http.get(uri).pipe(
+      map(res => {
+        return res;
+      })
+    );
+  }
+
+  updateCoin(name, price, id) {
+    const uri = 'http://localhost:4000/coins/update/' + id;
+
+    const obj = {
+      name: name,
+      price: price
+    };
+    this.http.post(uri, obj).subscribe(res => console.log('Done'));
+  }
+
+  deleteCoin(id) {
+    const uri = 'http://localhost:4000/coins/delete/' + id;
+
+    return this.http.get(uri).pipe(
+      map(res => {
+        return res;
+      })
+    );
+  }
+}
