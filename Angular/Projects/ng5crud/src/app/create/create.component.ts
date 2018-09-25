014b902ad94e5f2a526d8897a45278587106ef52
Steven <stevenxchung@gmail.com>
2018-09-25 18:46:22 -0400

    Update create controller

--- Angular/Projects/ng5crud/src/app/create/create.component.ts
+++ Angular/Projects/ng5crud/src/app/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CoinService } from '../coin.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  title = 'Add Coin';
+  angForm: FormGroup;
+  constructor(private coinservice: CoinService, private fb: FormBuilder) {
+    this.createForm();
   }
-
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+  }
+  addCoin(name, price) {
+    this.coinservice.addCoin(name, price);
+  }
+  ngOnInit() {}
 }
