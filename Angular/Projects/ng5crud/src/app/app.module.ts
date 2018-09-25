ed7b7e027f3dc58cabd08122394ec6bf0c0b5449
Steven <stevenxchung@gmail.com>
2018-09-25 18:44:46 -0400

    Import ReactiveFormsModule

--- Angular/Projects/ng5crud/src/app/app.module.ts
+++ Angular/Projects/ng5crud/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routerConfig';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -12,7 +13,12 @@ import { CoinService } from './coin.service';
 
 @NgModule({
   declarations: [AppComponent, IndexComponent, CreateComponent, EditComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [CoinService],
   bootstrap: [AppComponent]
 })
