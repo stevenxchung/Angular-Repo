4ad3c8a2abd23d56b2ca9a752ec0c4992d651793
Steven <stevenxchung@gmail.com>
2018-09-26 18:38:45 -0400

    Add edit controller

--- Angular/Projects/ng5crud/src/app/edit/edit.component.ts
+++ Angular/Projects/ng5crud/src/app/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CoinService } from '../coin.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  coin: any;
+  angForm: FormGroup;
+  title = 'Edit Coin';
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: CoinService,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
   }
 
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+  }
+
+  updateCoin(name, price) {
+    this.route.params.subscribe(params => {
+      this.service.updateCoin(name, price, params['id']);
+      this.router.navigate(['index']);
+    });
+  }
+
+  deleteCoin(id) {
+    this.service.deleteCoin(id).subscribe(res => {
+      console.log('Deleted');
+    });
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.coin = this.service.editCoin(params['id']).subscribe(res => {
+        this.coin = res;
+      });
+    });
+  }
 }
